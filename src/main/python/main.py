6aa53205691ca4105bb575c6cf1ac7bf851c96a5
slawa <slawa@slawagurevich.com>
2021-01-17 21:55:57 +0100

    First design draft

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -3,9 +3,17 @@ from PyQt5.QtWidgets import QMainWindow
 
 import sys
 
+from MainWindow import Ui_MainWindow
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, obj=None, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+
 if __name__ == '__main__':
     appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    window = QMainWindow()
+    window = MainWindow()
     window.resize(250, 150)
     window.show()
     exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
